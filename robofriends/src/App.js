db14541d94e1b961296d200e81eb80ae30dfcb61
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 13:09:30 -0400

    Added a icon and changed the project titie

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react'
 import Card from './components/card/Card'
 import { robots } from './Robots'
 
 const App = () => (
   <>
     {robots.map((robot) => (
-
       <Card key={robot.id} name={robot.name} email={robot.email} />
     ))}
   </>
--- robofriends/src/components/card/Card.js
+++ robofriends/src/components/card/Card.js
@@ -1,7 +1,6 @@
 
 const Card = ({ name, email }) => (
   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow5">
-    <h1>RoboFriends</h1>
     <img src='' alt="Card img" height="200px" width="200px" />
     <div>
       <h2>{name} </h2>
