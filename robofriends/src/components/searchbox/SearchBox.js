4b5cfebc3c04f1523de4616dd5e056d6923fefc6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 14:31:27 -0400

    Added styling to the searchbox component

--- robofriends/src/components/searchbox/SearchBox.js
+++ robofriends/src/components/searchbox/SearchBox.js
@@ -1,6 +1,8 @@
 
 const SearchBox = () => (
-  <input type="text" placeholder="Search for Robots" />
+  <div className="pa2 tc">
+    <input className="pa3 ba b--green bg-lightest-blue" type="text" placeholder="Search for Robots" />
+  </div>
 )
 
 export default SearchBox
