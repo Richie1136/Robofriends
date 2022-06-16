5456cf174a41a2c211c67fcb48c4593c3df30e1c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 14:25:34 -0400

    Created a searchbox component and imported it into the App.js file

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,10 +1,12 @@
 import CardList from './components/cardlist/CardList'
 import { robots } from './Robots'
+import SearchBox from './components/searchbox/SearchBox'
 
 
 const App = () => (
   <>
     <h1 className='tc bg-blue red'>RoboFriends</h1>
+    <SearchBox />
     <CardList robots={robots} />
   </>
 )
---
+++ robofriends/src/components/searchbox/SearchBox.js
@@ -0,0 +1,6 @@
+
+const SearchBox = () => (
+  <input type="text" placeholder="Search for Robots" />
+)
+
+export default SearchBox
