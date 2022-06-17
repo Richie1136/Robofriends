7cbe938651a51c9fe20ec17718e2ec864ed9b3d5
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-17 11:50:21 -0400

    Moved the filter on the robot state outside of the onSearchChange function because i need to pass it as props to the cardList

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -5,19 +5,26 @@ import { useState } from 'react'
 
 console.log(robots)
 
+
+
 const App = () => {
-  const [Robots, setRobots] = useState(robots)
+  const [robot, setRobot] = useState(robots)
   const [searchField, setSearchField] = useState("")
+
+  let filtered = robot.filter((ro) => ro.name.toLowerCase().includes(searchField.toLowerCase()))
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value)
+
   }
 
   return (
+
     <>
+
       <h1 className='tc bg-blue red'>RoboFriends</h1>
       <SearchBox onSearchChange={onSearchChange} />
-      <CardList robots={Robots} />
+      <CardList robots={filtered} />
     </>
   )
 }
