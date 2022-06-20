4d31b16bc579eb21a832d58f71d016f632b14c5b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-20 13:54:27 -0400

    Fetching data from an api and displaying the robots onto the page

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,14 +1,24 @@
 import CardList from './components/cardlist/CardList'
 import { robots } from './Robots'
 import SearchBox from './components/searchbox/SearchBox'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
-  const [robot, setRobot] = useState(robots)
   const [searchField, setSearchField] = useState("")
 
-  let filtered = robot.filter((ro) => ro.name.toLowerCase().includes(searchField.toLowerCase()))
+  const [getData, setgetData] = useState([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setgetData(data)
+    }
+    getUsers()
+  }, [])
+
+  let filtered = getData.filter((ro) => ro.name.toLowerCase().includes(searchField.toLowerCase()))
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value)
--- robofriends/src/components/cardlist/CardList.js
+++ robofriends/src/components/cardlist/CardList.js
@@ -1,6 +1,7 @@
 import Card from '../card/Card'
 
 const CardList = ({ robots }) => (
+
   <>
     {robots.map((robot, i) => (
       <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
