7f36be0be0ab9145447fe22e85c41cfa85414143
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-21 13:37:38 -0400

    Adding a try catch block inside of my useEffect hook

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -11,9 +11,13 @@ const App = () => {
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      const data = await response.json()
-      setgetData(data)
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setgetData(data)
+      } catch (error) {
+        console.log(error)
+      }
     }
     getUsers()
   }, [])
